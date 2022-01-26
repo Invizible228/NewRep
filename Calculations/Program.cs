c959e0c897840ced20f5d00fdfbe8c13b777c92c
Inivizible228 <Sulaymanov_tima@mail.ru>
2022-01-26 12:19:30 +0500

    Multiplication

---
+++ Calculations/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите A:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите B:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите цифру, что хотите сделать: 1 - '-', 2 - '+', 3 - '*', 4 - '/'");
+int sw = Convert.ToInt32(Console.ReadLine());
+switch (sw)
+{
+    //case 1:
+    //    Console.WriteLine(a - b);
+    //    break;
+    //case 2: Console.WriteLine(a + b);
+    //    break;
+    case 3:
+        Console.WriteLine(a * b);
+        break;
+        //case 4: Console.WriteLine(a / b);
+        //    break;
+}
